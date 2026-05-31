352c8c86a097ca913d4059cf88a448f40702d022
albertosilaen <albertosln31@gmail.com>
2026-05-31 23:24:15 +0700

    force redeploy

--- next.config.js
+++ next.config.js
@@ -1,12 +1,7 @@
 ﻿/** @type {import('next').NextConfig} */
 const nextConfig = {
-    allowedDevOrigins: [
-        "192.168.1.26",   // ← IP HP kamu
-        "192.168.1.*",    // ← atau pakai wildcard untuk semua device di jaringan yang sama
-    ],
-    env: {
-        NEXT_PUBLIC_API_URL: "http://192.168.1.26:5000",  // ← tambah ini
-    },
+    // force redeploy
+    allowedDevOrigins: ["192.168.*.*"],
 };
 
 module.exports = nextConfig;
